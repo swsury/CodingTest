e59f8cf017cd20b788ba530b477376920aa00fa5
Sury <sury.epm@gmail.com>
2024-01-11 11:25:14 +0900

    pass

--- SW_Expert_Academy/src/level_1/java_1993.java
+++ SW_Expert_Academy/src/level_1/java_1993.java
@@ -8,38 +8,10 @@ public class java_1993 {
 		
 		Scanner sc = new Scanner(System.in);
 		int num = sc.nextInt();
-		int one = 1;
-		int n = num;
 		
-		int i=2;
-		ArrayList<Integer> list = new ArrayList<>();
-		
-		while(num>=i) 
-		{
-			if(num%i==0) 
-			{
-				list.add(i);
-				num /= i;
-			}
-			else 
-			{
-				i++;
-			}
-		}
-		
-		HashSet<Integer> arr = new HashSet<Integer>(list);
-		
-		String resert = arr.toString().replaceAll(",", "");
-		String answer = resert.replace("[", "").replace("]", "");
-		
-		int lastIdx = list.size() -1; 
-		int lastNum = list.get(lastIdx);
-		
-		if (lastNum == n) {
-			System.out.println(one +" "+ answer);
-		}
-		else {
-			System.out.println(one +" "+ answer +" "+ n);
+		for (int i = 1; i <= num; i++ ) {
+			if ( num%i != 0) continue;
+			System.out.format("%d ", i);
 		}
 		
 		
